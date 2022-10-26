c6b85172f7177b324b7aac77f4c69af15d6f61bb
Keelen Fisher <keelencarrera@gmail.com>
2022-10-26 02:08:04 -0500

    url call changed and knowledge gained in useState()

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -4,22 +4,19 @@ import './form.scss';
 // Expects a function to be sent to it as a prop
 function Form(props) {
 
-  let [restingMethod, selectedMethod] = useState('get');
+  let [method, setMethod] = useState('get');
 
-  const handleMethod = (e) => {
-    e.preventDefault();
-    selectedMethod(e.target.id);
-  }
+  // const handleMethod = (e) => {
+  //   e.preventDefault();
+  //   selectedMethod(e.target.id);
+  // }
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = {
-      method: restingMethod || 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    props.handleApiCall(formData);
-  };
+    let getUrl = e.target.url.value;
+    props.handleApiCall(getUrl, method);
+
 
   // Example from our standup
   // [counter, setCounter] = useState(0);
@@ -31,7 +28,7 @@ function Form(props) {
   
   //   }
   //   return [init, changeTheValue];
-  // }
+  }
 
   
 
@@ -44,8 +41,8 @@ function Form(props) {
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span data-testid="get-test" onClick={handleMethod} id="get">GET</span>
-          <span id="post">POST</span>
+        <span id="get" onClick={() => setMethod('GET')} >GET</span>
+          <span id="post" onClick={() => setMethod('POST')}>POST</span>
           <span id="put">PUT</span>
           <span id="delete">DELETE</span>
         </label>
